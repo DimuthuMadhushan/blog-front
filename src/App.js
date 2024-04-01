70a9a45cb16f450b9ed3ce1f119ae104b38b1449
Dimuthu Madhushan <dimuthuM@thesanmark.com>
2024-04-01 14:51:17 +0530

    created pages

--- src/App.js
+++ src/App.js
@@ -1,21 +1,11 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import BrowserRouter from 'react-router-dom'
 
-class App extends Component {
-  render() {
+function App () {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+    <BrowserRouter>
+      
+    </BrowserRouter>
     );
-  }
 }
 
 export default App;
